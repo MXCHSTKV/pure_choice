46798933211ec2d2f06c82a3f41c9966c040d4da
MXCHSTKV <chistiakovmax@gmail.com>
2024-07-22 18:51:47 +0200

    Removed typo

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="min-w-[370px] mt-auto sm:mx-2 rounded-t-xl bg-orange-400 text-gray-600 text-center">
-            Creater by Saitr
+            Created by Saitr
         </footer>
     )
 }
